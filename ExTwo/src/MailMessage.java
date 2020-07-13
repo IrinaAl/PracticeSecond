b30557df7768500ae2b4634a564e787386ab9ed8
IrinaAl <55242137+IrinaAl@users.noreply.github.com>
2020-07-13 18:43:15 +0700

    Delete MailMessage.java

--- ExTwo/src/MailMessage.java
+++
@@ -1,7 +0,0 @@
-//класс для работы с письмами
-public class MailMessage extends DopClass<String> {
-    public MailMessage(String sender, String recipient, String content) {
-        super(sender, recipient, content);
-    }
-}
-
