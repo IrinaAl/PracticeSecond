4cdd1e3517b88f8f9bc888895a3bf730139b86fd
IrinaAl <55242137+IrinaAl@users.noreply.github.com>
2020-07-13 18:43:26 +0700

    Delete Salary.java

--- ExTwo/src/Salary.java
+++
@@ -1,7 +0,0 @@
-//класс для работы с зарплатой
-public class Salary extends DopClass<Integer> {
-    public Salary(String sender, String recipient, Integer content) {
-        super(sender, recipient, content);
-    }
-}
-
